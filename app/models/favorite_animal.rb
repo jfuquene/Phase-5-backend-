687c68cfab7b34d8a53e7aaa7c7d6172ef6cc744
John Fuquene <jfuquene@email.wm.edu>
2021-04-13 14:38:08 -0400

    deleted models

--- app/models/favorite_animal.rb
+++
@@ -1,4 +0,0 @@
-class FavoriteAnimal < ApplicationRecord
-  belongs_to :user
-  belongs_to :animal
-end
--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-    has_many :favorite_animals
-    has_many :animals, through: :favorite_animals
-end
