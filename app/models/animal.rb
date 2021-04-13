083ad800fca411ee2392628b433a820837549fc3
John Fuquene <jfuquene@email.wm.edu>
2021-04-13 17:23:59 -0400

    tables

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,4 @@
 class Animal < ApplicationRecord
+    has_many :favorite_animal
+    has_many :users, through: :favorite_animal
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
    has_secure_password
-# validates :username, presence: true
-# validates :username, uniqueness: true
-# validates :username, length: { minimum: 4 }
-# validates :email, presence: true
-# validates :email, uniqueness: true
-# validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+   has_many :favorite_animals
+   has_many :animals, through: :favorite_animals
+    
+   
+   validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :username, length: { minimum: 4 }
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
